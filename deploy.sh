8edb219a4d70311d9eb3fa6abf1852442cb664a2
Oleksandr Boyarskii <oleksandr.boyarskii@iternal.group>
2022-10-06 14:29:17 +0300

    deploy config edit

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:AldoRayne/vue-typescript-learn.git main:gh-pages
+git push -f git@github.com:AldoRayne/vue-typescript-learn.git master:gh-pages
 cd -
