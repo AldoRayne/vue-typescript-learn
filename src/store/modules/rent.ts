16d69e80b3121949af2046d13f7b4d5117b0bdae
Oleksandr Boyarskii <oleksandr.boyarskii@iternal.group>
2022-10-11 11:05:33 +0300

    env add

--- src/store/modules/rent.ts
+++ src/store/modules/rent.ts
@@ -20,11 +20,15 @@ export default {
   },
   actions: {
     async getRents({ commit }: { commit: Commit }) {
-      const { data } = await axios.get(
-        `https://baza-lider.info/api/v1/?get=estate&format=json&type=rent&region=1,2&access_token=re5lSc7y333hXhWTZWulOPiJ7ZBbdwVUWdvQd0XGyx9qu8THAeyQDlBT2yIfJcKd`
-      );
+      try {
+        const { data } = await axios.get(
+          `https://baza-lider.info/api/v1/?get=estate&format=json&type=rent&region=1,2&access_token=${process.env.VUE_APP_TOKEN}`
+        );
 
-      commit("getRents", data);
+        commit("getRents", data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
--- src/store/modules/sale.ts
+++ src/store/modules/sale.ts
@@ -20,11 +20,15 @@ export default {
   },
   actions: {
     async getSales({ commit }: { commit: Commit }) {
-      const { data } = await axios.get(
-        `https://baza-lider.info/api/v1/?get=estate&format=json&type=sale&region=1,2&access_token=re5lSc7y333hXhWTZWulOPiJ7ZBbdwVUWdvQd0XGyx9qu8THAeyQDlBT2yIfJcKd`
-      );
+      try {
+        const { data } = await axios.get(
+          `https://baza-lider.info/api/v1/?get=estate&format=json&type=sale&region=1,2&access_token=${process.env.VUE_APP_TOKEN}`
+        );
 
-      commit("getSales", data);
+        commit("getSales", data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
