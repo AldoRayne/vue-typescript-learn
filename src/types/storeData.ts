d6c7621d9228882e83489ef927849da0e7cbe24b
Oleksandr Boyarskii <oleksandr.boyarskii@iternal.group>
2022-10-11 09:45:15 +0300

    keysof

--- src/types/storeData.ts
+++ src/types/storeData.ts
@@ -1,4 +1,17 @@
+interface allowedGetterNames {
+  rents: string;
+  sales: string;
+}
+
+interface allowedDispatchNames {
+  getRents: string;
+  getSales: string;
+}
+
+type getterNameKeys = keyof allowedGetterNames;
+type dispatchNameKeys = keyof allowedDispatchNames;
+
 export default interface StoreData {
-  getterName: string;
-  dispatchName: string;
+  getterName: getterNameKeys;
+  dispatchName: dispatchNameKeys;
 }
